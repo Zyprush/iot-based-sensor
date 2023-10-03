e76508ed415c5c858162ad9b84c4ffb6524d1748
Zyprush <alberiojake27@gmail.com>
2023-10-03 13:45:24 +0800

    try

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();  
 
 export default function App() {
   return (
